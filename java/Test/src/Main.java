6a3b8dd81f21eb53ba39fb8820f5780c11454904
bjha <bjha@outlook.in>
2019-10-02 09:50:42 +0530

    Sample code for SO question

---
+++ java/Test/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//https://stackoverflow.com/questions/58191577/i-tried-a-lot-of-different-things-in-order-for-the-program-to-work-i-am-trying
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (Scanner keyboard = new Scanner(System.in)) {
+
+			System.out.println("First int (length): ");
+			int L = keyboard.nextInt();
+
+			System.out.println("Second Int (height): ");
+			int H = keyboard.nextInt();
+
+			if ((L > 0 && H > 0) && (L < 21 && H < 21)) {
+
+				for (int j = 0; j < H; j++) {
+					for (int i = 0; i < L; i++) {
+						System.out.print("*");
+					}
+					System.out.println();
+				}
+
+				return;
+			}			
+		}
+		
+		System.out.println("Bad input. Please enter values between [1-21].");
+	}
+}
