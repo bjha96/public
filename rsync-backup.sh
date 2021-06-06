d7df958ca5dac0a9f725039f71df59d55c12fdfb
bjha <bjha@outlook.in>
2021-06-06 15:32:04 +0530

    rsync script added reference for SKIP compression list

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -40,7 +40,7 @@ fi
 
 RSYNC_OPTS="${RSYNC_OPTS} --include-from=${RSYNC_INC_LIST} --exclude=*"
 
-
+#Src: https://gist.github.com/StefanHamminga/2b1734240025f5ee916a
 RSYNC_SKIP_COMPRESS="3g2/3gp/3gpp/7z/aac/ace/amr/apk/appx/appxbundle/arc/arj/asf/avi/bz2/cab/crypt5/crypt7/crypt8/deb/dmg/drc/ear/gz/flac/flv/gpg/iso/jar/jp2/jpg/jpeg/lz/lzma/lzo/m4a/m4p/m4v/mkv/msi/mov/mp3/mp4/mpeg/mpg/mpv/oga/ogg/ogv/opus/pack/png/qt/rar/rpm/rzip/s7z/sfx/svgz/tbz/tgz/tlz/txz/vob/wim/wma/wmv/xz/z/zip/zst"
 
 
