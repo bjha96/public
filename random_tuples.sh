1483b336e8f10b03d2ed24379f5237ea57512fa1
Bimalesh Jha <37788879+bjha96@users.noreply.github.com>
2025-08-25 09:11:08 +0530

    Improved error handling

--- random_tuples.sh
+++ random_tuples.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Form random tuple (pair and triplets) from CSV file containing a list of items
-# like, for triplets:
+# Generate random tuple (pair and triplets) from CSV file containing list of items.
+# For example, conside CSV for triplets:
 # A,B,C
 # D,E,F
 # G,H,I
@@ -13,7 +13,7 @@
 set -eo pipefail
 
 if [[ "$#" -ne 2 ]]; then
-  echo "Usage: $(basename ${0}) list.csv <2|3> for forming random pairs or triplets."
+  echo "Usage: $(basename ${0}) <list.csv> <2|3> for generating random pairs or triplets."
   exit 1
 fi
 
@@ -40,6 +40,14 @@ if [[ "$tupCount" == "2" ]]; then
       col1+=("$first")
       col2+=("$second")
    done < "$csv_file"
+   
+   len1=${#col1[@]}
+   len2=${#col2[@]}
+   
+   if [[ "$len1" == "0" || "$len2" == "0" || "$len1" != "$len2" ]]; then
+       echo "Bad CSV input!"
+       exit 3
+   fi 
 
    # shuffle lists
    mapfile -t s_col1 < <(shuf -e "${col1[@]}")
@@ -64,6 +72,15 @@ elif [[ "$tupCount" == "3" ]]; then
 
    done < "$csv_file"
 
+   len1=${#col1[@]}
+   len2=${#col2[@]}
+   len3=${#col3[@]}
+   
+   if [[ "$len1" == "0" || "$len2" == "0" || "$len3" == "0" || "$len1" != "$len2" || "$len1" != "$len3" ]]; then
+       echo "Bad CSV input!"
+       exit 3
+   fi 
+   
    mapfile -t s_col1 < <(shuf -e "${col1[@]}")
    mapfile -t s_col2 < <(shuf -e "${col2[@]}")
    mapfile -t s_col3 < <(shuf -e "${col3[@]}")
@@ -72,5 +89,6 @@ elif [[ "$tupCount" == "3" ]]; then
       printf "%d: %s - %s - %s\n" "$idx" "${s_col1[$idx]}" "${s_col2[$idx]}" "${s_col3[$idx]}"
    done
 else
-   echo "Bad tuple order: $tupCount"
+   echo "Bad tuple order- ${tupCount}. Choose 2 or 3."
+   exit 1
 fi
